440c54bad5708e31038e84d52b62669b439d66c4
Radosław Wierzgała <rad.wierzgala@gmail.com>
2024-02-06 22:05:46 +0100

    Initial commit. GameMatch class defined.

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Board Game Recorder",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Radosław Wierzgała",
+    'category': 'Board Games',
+    'description': """
+    Board Game Recorder for Odoo.
+    """,
+
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ models/game_match.py
@@ -0,0 +1,4 @@
+from odoo import models
+
+class GameMatch(models.Model):
+    _name = "game.match"
