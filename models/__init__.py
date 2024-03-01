ae67d250b599f3a2ae6de42c1888b0411205e5c9
Radosław Wierzgała <rad.wierzgala@gmail.com>
2024-03-02 00:18:15 +0100

    -'Game Score Component' page added to Game Form View, -__init__.py updated, -relation field game_score_component_ids added to Game model

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from . import game_match_line_component
 from . import game_match_line
 from . import game
 from . import player
+from . import game_score_component
--- models/game.py
+++ models/game.py
@@ -9,6 +9,7 @@ class Game(models.Model):
     game_match_ids = fields.One2many('game.match', string="Game Matches", inverse_name="game_id")
     game_match_line_ids = fields.One2many('game.match.line', string="Game Matche Line", inverse_name="game_id", related="game_match_ids.game_match_line_ids")
     avg_score = fields.Float(string='Average Score', compute='_compute_avg_score', store=True)
+    game_score_component_ids = fields.One2many('game.score.component', string="Game Matche Line", inverse_name="game_id")
 
     #TODO add form view with readolny game_match_ids!!
     #TODO add game_match_line_ids and also to Form View.
