ea01d24f2c0e7b3ac743237f7727235ab919a473
Radosław Wierzgała <rad.wierzgala@gmail.com>
2024-03-02 00:06:02 +0100

    Game Score Component model added.

--- models/game_score_component.py
+++ models/game_score_component.py
@@ -1,19 +1,8 @@
 from odoo import models, fields, api, _
 
 
-class Game(models.Model):
-    _name = "game"
+class GameScoreComponent(models.Model):
+    _name = "game.score.component"
 
-    name = fields.Char(string="Name", help="Name of the game match")
-    note = fields.Char(string="Note", help="Note of the game match")
-    game_match_ids = fields.One2many('game.match', string="Game Matches", inverse_name="game_id")
-    game_match_line_ids = fields.One2many('game.match.line', string="Game Matche Line", inverse_name="game_id", related="game_match_ids.game_match_line_ids")
-    avg_score = fields.Float(string='Average Score', compute='_compute_avg_score')
-
-    #TODO add form view with readolny game_match_ids!!
-    #TODO add game_match_line_ids and also to Form View.
-
-    @api.depends("game_match_line_ids")
-    def _compute_avg_score(self):
-        scores = self.game_match_line_ids.mapped('score')
-        self.avg_score = sum(scores) / len(scores)
+    name = fields.Char(string="Name", help="Name of the Game Score Component")
+    game_id = fields.Many2one(comodel_name="game", string="Game")
